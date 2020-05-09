a5694c7652b716f8eee7d93bf78cdcc51df4bf55
Karol Trzaska <karoltrzaska19@gmail.com>
2020-05-09 14:36:32 +0200

    static methods and resolve reject

--- promise.ts
+++ promise.ts
@@ -5,15 +5,36 @@ const states = {
 }
 
 export class CustomPromise {
-    private state: string;
-    constructor(executor: (resolve, reject) => unknown) {
-        const resolve = () => {
+    state;
+    value;
+    constructor(executor) {
+        const members =  { 
+            [states.resolved]: {
+            state: states.resolved,
+            // Chain mechanism
+            then: onResolved => CustomPromise.resolve(onResolved(this.value))
+            },
+            [states.rejected]: {
+                state: states.rejected,
+                // Ignore mechanism
+                then: _ => this
+            },
+            [states.pending]: {
+                state: states.pending
+            }
+        };
+        const changeState = state => Object.assign(this, members[state]);
+        const resolve = (value) => {
             this.state = states.resolved;
+            changeState(this.state);
+            this.value = value;
         }
 
         const reject = (error) => {
             console.log(error);
             this.state = states.rejected;
+            changeState(this.state);
+
         }
 
         this.state = states.pending;
@@ -23,4 +44,18 @@ export class CustomPromise {
             reject(error);
         }
     }
+
+    static resolve(value) {
+        return new CustomPromise(resolve => resolve(value));
+    }
+
+    static reject(value) {
+        return new CustomPromise((_, reject) => reject(value));
+    }
 }
+
+const test = new CustomPromise((resolve, reject) => {
+
+});
+
+test.then()
