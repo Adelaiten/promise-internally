e988c165176e8a8707dd16528eab4da92e91dc0f
Karol Trzaska <karoltrzaska19@gmail.com>
2020-05-09 12:34:05 +0200

    promise class and constructor

--- promise.ts
+++ promise.ts
@@ -3,3 +3,24 @@ const states = {
     resolved: 'resolved',
     rejected: 'rejected'
 }
+
+export class CustomPromise {
+    private state: string;
+    constructor(executor) {
+        const resolve = () => {
+            this.state = states.resolved;
+        }
+
+        const reject = (error) => {
+            console.log(error);
+            this.state = states.rejected;
+        }
+
+        this.state = states.pending;
+        try {
+            executor(resolve, reject);
+        } catch(error) {
+            reject(error);
+        }
+    }
+}
